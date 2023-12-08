52c028ad8fff2db467d210b30c789e73b16e511d
gukazma <gukazma@proton.me>
2023-12-08 22:04:09 +0800

    🔧 build: link openmvs

--- conanfile.py
+++ conanfile.py
@@ -18,6 +18,11 @@ class ModuleConan(ConanFile):
     def requirements(self):
         self.requires("gtest/1.14.0")
         self.requires("qt/5.15.3")
+        self.requires("openmvg/2.0")
+        self.requires("zlib/1.3", override=True)
+        self.requires("libjpeg/9e", override=True)
+        self.requires("libpng/1.6.40", override=True)
+        self.requires("zstd/1.5.5", override=True)
         
     def imports(self):
         self.copy("*.dll", "./bin", "bin")
